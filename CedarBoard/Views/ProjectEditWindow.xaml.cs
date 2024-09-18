3f3061334e91eeb6915027d78ff55fa5e966770e
kyonsy <kyonshi297@gmail.com>
2024-09-18 09:29:04 +0900

    プロジェクト ファイルを追加します。

---
+++ CedarBoard/Views/ProjectEditWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CedarBoard.Views
+{
+    /// <summary>
+    /// Interaction logic for ProjectEditWindow
+    /// </summary>
+    public partial class ProjectEditWindow : UserControl
+    {
+        public ProjectEditWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CedarBoard/Views/SolutionCreateWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace CedarBoard.Views
+{
+    /// <summary>
+    /// Interaction logic for SolutionCreateWindow.xaml
+    /// </summary>
+    public partial class SolutionCreateWindow : Window
+    {
+        public SolutionCreateWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

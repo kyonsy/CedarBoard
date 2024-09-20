38b2104113ee4ec751fc792acbdab84c6408b4cf
y.rait <y.raito15926535@gmail.com>
2024-09-20 16:43:09 +0900

    実験を直した

--- Main/Views/ProjectEditWindow.xaml.cs
+++ Main/Views/ProjectEditWindow.xaml.cs
@@ -11,9 +11,5 @@ namespace Main.Views
         {
             InitializeComponent();
         }
-        private void Button_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            MessageBox.Show("何で押したん？");
-        }
     }
 }

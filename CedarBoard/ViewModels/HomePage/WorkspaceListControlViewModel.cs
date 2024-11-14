0e2729aba56fb3454917c74fdc9bc050621ee580
kyonsy <kyonshi297@gmail.com>
2024-11-14 23:43:35 +0900

    やっぱりPageをコントロールに変える とりあえず新しいbranchを切ってからWorkSpaceListControlの実装だけ行った

--- CedarBoard/ViewModels/HomePage/WorkspaceListControlViewModel.cs
+++ CedarBoard/ViewModels/HomePage/WorkspaceListControlViewModel.cs
@@ -2,15 +2,36 @@
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace CedarBoard.ViewModels.HomePage
 {
     public class WorkspaceListControlViewModel : BindableBase
     {
-        public WorkspaceListControlViewModel()
-        {
 
+        // ObservableCollectionをプロパティとして定義
+        public ObservableCollection<KeyValuePair<string, string>> _dictionaryItems;
+        public ObservableCollection<KeyValuePair<string, string>> DictionaryItems
+        {
+            get { return _dictionaryItems; }
+            set { SetProperty(ref _dictionaryItems, value); }
+        }
+
+
+        public WorkspaceListControlViewModel(HomeControlViewModel _homeControlViewModel)
+        {
+            // テストコード
+            //DictionaryItems = new ObservableCollection<KeyValuePair<string, string>>(
+            //    new Dictionary<string, string>
+            //    {
+            //        { "apple","200"},
+            //        {"orange","300"},
+            //        { "strbery","400"}
+            //    });
+
+            DictionaryItems = new ObservableCollection<KeyValuePair<string, string>>(
+                _homeControlViewModel.WorkspaceSelector.SelectorPoco.PathDictionary);
         }
     }
 }

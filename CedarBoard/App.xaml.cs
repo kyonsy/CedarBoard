a8a90a26ce77c214975ce1c419d0604167e67ce3
kyonsy <kyonshi297@gmail.com>
2025-01-12 14:24:30 +0900

    とりまコミット

--- CedarBoard/App.xaml.cs
+++ CedarBoard/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using CedarBoard.Views;
 using Prism.Ioc;
 using System.Windows;
+using CedarBoard.Model;
+using CedarBoard.Model.Accessor;
 
 namespace CedarBoard
 {
@@ -24,7 +26,24 @@ namespace CedarBoard
         /// <param name="containerRegistry"></param>
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            
+            // テスト用にシングルトンにはモックを登録しておく
+            containerRegistry.RegisterSingleton<WorkspaceSelector>(() =>
+            {
+                WorkspaceSelector sel = new(new TextFileMock(), new DirectoryMock()) { SelectorPoco = new() { PathDictionary = [] } };
+                sel.Add(new()
+                {
+                    Author = "kyonsy",
+                    Editor = "notepad",
+                    Encode = "UTF-8",
+                    Format = "default",
+                    Language = "ja",
+                    Name = "hogehoge",
+                    CreatedDate = "2024/10/24",
+                    UpdatedDate = "2024/10/24",
+                    Message = "始めに作ったやつ"
+                }, "C:");
+                return sel;
+            });
         }
     }
 }

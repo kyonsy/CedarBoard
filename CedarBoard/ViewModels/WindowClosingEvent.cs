3cdb5e8f5194596d4812a763a4658caf06fabc4e
kyonsy <kyonshi297@gmail.com>
2025-01-28 18:32:34 +0900

    あああ

---
+++ CedarBoard/ViewModels/WindowClosingEvent.cs
@@ -0,0 +1,21 @@
+// Copyright (c) 2024 YourName
+// MIT License
+// 詳細は LICENSE ファイルを参照してください。
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CedarBoard.ViewModels
+{
+    /// <summary>
+    /// ウィンドウが閉じる際のイベントの定義
+    /// </summary>
+    public class WindowClosingEvent: PubSubEvent<CancelEventArgs>
+    {
+    }
+}
+
